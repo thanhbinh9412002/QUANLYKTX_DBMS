c33ea5f6f526f2952582397d17b712de97002b00
Thanh Bình <95224034+thanhbinh9412002@users.noreply.github.com>
2022-11-14 09:57:44 +0700

    Add files via upload

---
+++ QuanLyKTX/QuanLyKTX/Data Provider/DataProvider.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace QuanLyKTX.DataProvider
+{
+
+    public class DBConnection
+    {
+        //SqlConnection cnnStr = new SqlConnection(@"Data Source=THANHBINH\SQLEXPRESS;Initial Catalog=QuanLyKTX;Integrated Security=True");
+        private SqlDataAdapter adapter;
+        private SqlConnection connection;
+        public DBConnection()
+        {
+            adapter = new SqlDataAdapter();
+            connection = new SqlConnection(Properties.Settings.Default.cnnStr);
+        }
+        private SqlConnection openConnection()
+        {
+            if (connection.State == ConnectionState.Closed || connection.State == ConnectionState.Broken)
+            {
+                connection.Open();
+            }
+            return connection;
+        }
+
+        public void executeInsertQuery(String query, SqlParameter[] sqlParameter)
+        {
+            using (SqlCommand sqlCommand = new SqlCommand(query, openConnection()))
+            {
+                sqlCommand.CommandType = CommandType.Text;
+                sqlCommand.Parameters.AddRange(sqlParameter);
+                try
+                {
+                    sqlCommand.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+        }
+
+        public object executeScalar(String query, CommandType storedProcedure, SqlParameter[] sqlParameter)
+        {
+            using (SqlCommand sqlCommand = new SqlCommand(query, openConnection()))
+            {
+                sqlCommand.CommandType = CommandType.Text;
+                sqlCommand.Parameters.AddRange(sqlParameter);
+                object result = new object();
+                try
+                {
+                    result = sqlCommand.ExecuteScalar();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+                return result;
+            }
+        }
+        public DataTable executeReader(String query, SqlParameter[] sqlParameter)
+        {
+            using (SqlCommand sqlCommand = new SqlCommand(query, openConnection()))
+            {
+                sqlCommand.CommandType = CommandType.Text;
+                sqlCommand.Parameters.AddRange(sqlParameter);
+                DataTable dt = new DataTable();
+                try
+                {
+                    SqlDataReader dataReader = sqlCommand.ExecuteReader(CommandBehavior.CloseConnection);
+                    dt.Load(dataReader);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+                return dt;
+            }
+        }
+        public int executeCount(string query)           // trả về số lượng 
+        {
+            int cout = 0;
+            using (SqlCommand sqlCommand = new SqlCommand(query, openConnection()))
+            {
+                try
+                {
+                    cout = (Int32)sqlCommand.ExecuteScalar();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+                return cout;
+            }
+        }
+
+        public DataTable executeLoadData(string query)      // trả về bảng dữ liệu
+        {
+            using (SqlCommand command = new SqlCommand(query, openConnection()))
+            {
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    DataTable dt = new DataTable();
+                    try
+                    {
+                        dt.Load(reader);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                    finally
+                    {
+                        connection.Close();
+                        reader.Close();
+                    }
+                    return dt;
+                }
+            }
+        }
+
+        public int ExecuteStoredProcedure(string spName, string[] pNames, object[] pValues) //trả về 1 giá trị khi gọi procedure bên sql
+
+        {
+            int kq = 0;
+            openConnection();
+            // Khai báo và khởi tạo đối tượng Command với tham số tên thủ tục spName
+            SqlCommand cmd = new SqlCommand(spName, connection);
+            // Khai báo kiểu thủ tục
+            cmd.CommandType = CommandType.StoredProcedure;
+            // Khai báo tham số SqlParameter
+            SqlParameter p;
+            // Khởi tạo danh sách các tham số với giá trị tương ứng
+            for (int i = 0; i < pNames.Length; i++)
+            {
+                p = new SqlParameter(pNames[i], pValues[i]);
+                cmd.Parameters.Add(p);
+            }
+            SqlParameter n = new SqlParameter("@kq", SqlDbType.Int);
+            n.Direction = ParameterDirection.ReturnValue;
+            cmd.Parameters.Add(n);
+            cmd.ExecuteScalar();
+            kq = (Int32)n.Value;
+            connection.Close();
+            return kq;
+        }
+
+        public int ExecuteStoredProcedure_Update(string spName, string[] pNames, object[] pValues) //thêm, xóa, sửa khi gọi procedure bên sql
+
+        {
+            openConnection();
+            // Khai báo và khởi tạo đối tượng Command với tham số tên thủ tục spName
+            SqlCommand cmd = new SqlCommand(spName, connection);
+            // Khai báo kiểu thủ tục
+            cmd.CommandType = CommandType.StoredProcedure;
+            // Khai báo tham số SqlParameter
+            SqlParameter p;
+            // Khởi tạo danh sách các tham số với giá trị tương ứng
+            for (int i = 0; i < pNames.Length; i++)
+            {
+                p = new SqlParameter(pNames[i], pValues[i]);
+                cmd.Parameters.Add(p);
+            }
+            connection.Close();
+            return cmd.ExecuteNonQuery(); 
+        }
+    }     
+}
---
+++ QuanLyKTX/QuanLyKTX/Form/frm_NhanVien.Designer.cs
@@ -0,0 +1,249 @@
+﻿
+namespace QuanLyKTX
+{
+    partial class frm_NhanVien
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvNhanVien = new System.Windows.Forms.DataGridView();
+            this.MaNhanVien = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TenNhanVien = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SoDienThoai = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.GioiTinh = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TonGiao = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.QuocTich = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CMND_CCCD = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ChucVu = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.MaNQL = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_quaylai = new System.Windows.Forms.Button();
+            this.btn_xoa = new System.Windows.Forms.Button();
+            this.btn_sua = new System.Windows.Forms.Button();
+            this.btn_them = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvNhanVien)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvNhanVien
+            // 
+            this.dgvNhanVien.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvNhanVien.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dgvNhanVien.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dgvNhanVien.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvNhanVien.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.MaNhanVien,
+            this.TenNhanVien,
+            this.SoDienThoai,
+            this.GioiTinh,
+            this.TonGiao,
+            this.QuocTich,
+            this.CMND_CCCD,
+            this.ChucVu,
+            this.MaNQL});
+            this.dgvNhanVien.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.dgvNhanVien.GridColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.dgvNhanVien.Location = new System.Drawing.Point(12, 50);
+            this.dgvNhanVien.Name = "dgvNhanVien";
+            this.dgvNhanVien.Size = new System.Drawing.Size(928, 266);
+            this.dgvNhanVien.TabIndex = 0;
+            this.dgvNhanVien.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvNhanVien_CellClick);
+            // 
+            // MaNhanVien
+            // 
+            this.MaNhanVien.DataPropertyName = "MaNhanVien";
+            this.MaNhanVien.HeaderText = "Mã nhân viên";
+            this.MaNhanVien.Name = "MaNhanVien";
+            this.MaNhanVien.Width = 90;
+            // 
+            // TenNhanVien
+            // 
+            this.TenNhanVien.DataPropertyName = "TenNhanVien";
+            this.TenNhanVien.HeaderText = "Tên nhân viên";
+            this.TenNhanVien.Name = "TenNhanVien";
+            this.TenNhanVien.Width = 120;
+            // 
+            // SoDienThoai
+            // 
+            this.SoDienThoai.DataPropertyName = "SoDienThoai";
+            this.SoDienThoai.HeaderText = "Số điện thoại";
+            this.SoDienThoai.Name = "SoDienThoai";
+            // 
+            // GioiTinh
+            // 
+            this.GioiTinh.DataPropertyName = "GioiTinh";
+            this.GioiTinh.HeaderText = "Giới tính";
+            this.GioiTinh.Name = "GioiTinh";
+            // 
+            // TonGiao
+            // 
+            this.TonGiao.DataPropertyName = "TonGiao";
+            this.TonGiao.HeaderText = "Tôn giáo";
+            this.TonGiao.Name = "TonGiao";
+            // 
+            // QuocTich
+            // 
+            this.QuocTich.DataPropertyName = "QuocTich";
+            this.QuocTich.HeaderText = "Quốc tịch";
+            this.QuocTich.Name = "QuocTich";
+            // 
+            // CMND_CCCD
+            // 
+            this.CMND_CCCD.DataPropertyName = "CMND_CCCD";
+            this.CMND_CCCD.HeaderText = "Số CMND/CCCD";
+            this.CMND_CCCD.Name = "CMND_CCCD";
+            // 
+            // ChucVu
+            // 
+            this.ChucVu.DataPropertyName = "ChucVu";
+            this.ChucVu.HeaderText = "Chức vụ";
+            this.ChucVu.Name = "ChucVu";
+            // 
+            // MaNQL
+            // 
+            this.MaNQL.DataPropertyName = "MaNQL";
+            this.MaNQL.HeaderText = "Mã người quản lý";
+            this.MaNQL.Name = "MaNQL";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(354, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(361, 38);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "NHÂN VIÊN";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btn_quaylai
+            // 
+            this.btn_quaylai.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btn_quaylai.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_quaylai.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_quaylai.Image = global::QuanLyKTX.Properties.Resources.back_icon;
+            this.btn_quaylai.Location = new System.Drawing.Point(676, 352);
+            this.btn_quaylai.Name = "btn_quaylai";
+            this.btn_quaylai.Size = new System.Drawing.Size(90, 52);
+            this.btn_quaylai.TabIndex = 5;
+            this.btn_quaylai.Text = "QUAY LẠI";
+            this.btn_quaylai.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btn_quaylai.UseVisualStyleBackColor = false;
+            this.btn_quaylai.Click += new System.EventHandler(this.btn_quaylai_Click);
+            // 
+            // btn_xoa
+            // 
+            this.btn_xoa.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btn_xoa.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_xoa.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_xoa.Image = global::QuanLyKTX.Properties.Resources.remove_icon;
+            this.btn_xoa.Location = new System.Drawing.Point(537, 352);
+            this.btn_xoa.Name = "btn_xoa";
+            this.btn_xoa.Size = new System.Drawing.Size(90, 52);
+            this.btn_xoa.TabIndex = 4;
+            this.btn_xoa.Text = "XÓA";
+            this.btn_xoa.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btn_xoa.UseVisualStyleBackColor = false;
+            this.btn_xoa.Click += new System.EventHandler(this.btn_xoa_Click);
+            // 
+            // btn_sua
+            // 
+            this.btn_sua.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btn_sua.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_sua.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_sua.Image = global::QuanLyKTX.Properties.Resources.setting;
+            this.btn_sua.Location = new System.Drawing.Point(394, 352);
+            this.btn_sua.Name = "btn_sua";
+            this.btn_sua.Size = new System.Drawing.Size(90, 52);
+            this.btn_sua.TabIndex = 3;
+            this.btn_sua.Text = "SỬA";
+            this.btn_sua.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btn_sua.UseVisualStyleBackColor = false;
+            this.btn_sua.Click += new System.EventHandler(this.btn_sua_Click);
+            // 
+            // btn_them
+            // 
+            this.btn_them.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btn_them.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_them.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_them.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_them.Image = global::QuanLyKTX.Properties.Resources.add_icon;
+            this.btn_them.Location = new System.Drawing.Point(256, 352);
+            this.btn_them.Name = "btn_them";
+            this.btn_them.Size = new System.Drawing.Size(90, 52);
+            this.btn_them.TabIndex = 2;
+            this.btn_them.Text = "THÊM";
+            this.btn_them.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btn_them.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btn_them.UseVisualStyleBackColor = false;
+            this.btn_them.Click += new System.EventHandler(this.btn_them_Click);
+            // 
+            // frm_NhanVien
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SteelBlue;
+            this.ClientSize = new System.Drawing.Size(952, 416);
+            this.Controls.Add(this.btn_quaylai);
+            this.Controls.Add(this.btn_xoa);
+            this.Controls.Add(this.btn_sua);
+            this.Controls.Add(this.btn_them);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvNhanVien);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frm_NhanVien";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Nhân viên";
+            this.Load += new System.EventHandler(this.frm_NhanVien_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvNhanVien)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvNhanVien;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_them;
+        private System.Windows.Forms.Button btn_sua;
+        private System.Windows.Forms.Button btn_xoa;
+        private System.Windows.Forms.Button btn_quaylai;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MaNhanVien;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TenNhanVien;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SoDienThoai;
+        private System.Windows.Forms.DataGridViewTextBoxColumn GioiTinh;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TonGiao;
+        private System.Windows.Forms.DataGridViewTextBoxColumn QuocTich;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CMND_CCCD;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ChucVu;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MaNQL;
+    }
+}
